718a96a18d5e52924d4f694689dafe3c227f08a4
mustechza <141557155+mustechza@users.noreply.github.com>
2025-05-09 16:29:04 +0300

    Update signal_strategies.py

--- signal_strategies.py
+++ signal_strategies.py
@@ -1,1 +1,12 @@
 
+import pandas_ta as ta
+
+def ema_crossover_signal(df):
+    df['ema_fast'] = ta.ema(df['close'], length=5)
+    df['ema_slow'] = ta.ema(df['close'], length=13)
+
+    if df['ema_fast'].iloc[-2] < df['ema_slow'].iloc[-2] and df['ema_fast'].iloc[-1] > df['ema_slow'].iloc[-1]:
+        return "BUY"
+    elif df['ema_fast'].iloc[-2] > df['ema_slow'].iloc[-2] and df['ema_fast'].iloc[-1] < df['ema_slow'].iloc[-1]:
+        return "SELL"
+    return None
